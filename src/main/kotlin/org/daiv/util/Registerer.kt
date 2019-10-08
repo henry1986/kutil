42cd8181ea5b7ed24f51cf64ab18a7c40e26a762
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2019-10-08 22:06:01 +0200

    source code added to build, new unregisterAll Function in registerer

--- src/main/kotlin/org/daiv/util/Registerer.kt
+++ src/main/kotlin/org/daiv/util/Registerer.kt
@@ -3,14 +3,24 @@ package org.daiv.util
 interface Registerer<T : Any> {
     fun register(t: T): Boolean
 
+    fun unregisterAll()
+
     fun unregister(t: T)
 }
 
 class DefaultRegisterer<T : Any>(private val list: MutableList<T> = mutableListOf()) : Registerer<T>,
-    Iterable<T> by list {
+                                                                                       Iterable<T> by list {
     override fun register(t: T) = list.add(t)
 
     override fun unregister(t: T) {
         list.remove(t)
+    }
+
+    override fun unregisterAll() {
+        list.clear()
+    }
+
+    fun event(func: T.() -> Unit) {
+        list.forEach(func)
     }
 }

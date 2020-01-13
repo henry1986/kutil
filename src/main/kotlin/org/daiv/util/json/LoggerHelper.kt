962c3c3ec476d0d1eac7aacd42f6141865f083d9
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2020-01-13 16:56:08 +0100

    new kotlin version 1.3.61

--- src/main/kotlin/org/daiv/util/json/LoggerHelper.kt
+++ src/main/kotlin/org/daiv/util/json/LoggerHelper.kt
@@ -2,9 +2,7 @@ package org.daiv.util.json
 
 import mu.KLogger
 
-fun <T> Iterable<T>.log(logger: KLogger, name: String = "") {
+fun <T> Iterable<T>.log(logger: KLogger, name: String = "", block: (Int, T) -> Unit = { i, e -> logger.trace { "$i: $e" } }) {
     logger.trace { "log $name" }
-    forEachIndexed { i, e ->
-        logger.trace { "$i: $e" }
-    }
+    forEachIndexed(block)
 }

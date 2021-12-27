d3712397a55cda89c1f9912801d4df96cec08f0e
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2021-12-27 13:49:04 +0100

    fixed java 17 problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven("https://artifactory.daiv.org/artifactory/gradle-dev-local")
     }
     dependencies {
-        classpath("org.daiv.dependency:DependencyHandling:0.1.0")
+        classpath("org.daiv.dependency:DependencyHandling:0.1.7")
     }
 }
 
